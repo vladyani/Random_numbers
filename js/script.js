80df48d7d9a6f875600c4d017f2c2d132e02fb8a
Vlad <vladyslavyantsov@gmail.com>
2018-03-25 17:25:58 +0200

    counter done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const result = [];
+
+function lotery() {
+    if (result.length === 6){
+        return;
+    }
+
+    const resultLotery = Math.floor(Math.random() * 49 + 1); //losuje nam liczbe losowa od 1 do 49 
+
+    for (let i = 0; i < result.length; i++) { //jesli uzyskamy te same liczby w losowaniu zatrzymaj petle i zwroc od nowa nasza funkcje losowanie 
+        if (resultLotery === result[i]) { //warunek jesli liczba losowana jest rowna tej w tablicy to wykonaj nastepujaca instrukcje czyli zwroc funkcje lotery() od nowa
+            console.log("ta sama liczba");
+            return lotery();
+        }
+    }
+
+    const div = document.createElement('div'); //dostajemy sie do obiektu document i dodajemy nowy element do html 
+    div.textContent = resultLotery;
+    document.body.appendChild(div); //w tej linijce dostajemy sie do document potem do body i tam dodajemy mu jako dziecko appendChild(div)  któ®y stworzylismy 
+    result.push(resultLotery); //dodajemy nasz div do 6 do tablicy z wyników siedzi w zmiennej result
+}
+
+
+//pobranie elementu na który pracujemy
+const button = document.querySelector('button');
+//nasluchiwwanie na zdarzenie w tym przypadku na kliknicie
+button.addEventListener("click", lotery);
